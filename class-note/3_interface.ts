c8828ea736248a201591dc9a0cf985a62ea25c44
dlstodyo <dudgks3316@naver.com>
2022-01-03 12:45:43 +0900

    interface

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -55,8 +55,12 @@ interface Person {
     name: string;
     age: number;
 }
-interface Person {
-    name: string;
-    age: number;
+interface Developer extends Person{
     language: string;
+}
+
+var captain: Developer = {
+    language: 'ts',
+    age: 100,
+    name: '캡틴'
 }
