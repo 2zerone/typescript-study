f676bc36804f3877f6d4ca6a2eb27ae107037248
dlstodyo <dudgks3316@naver.com>
2022-01-30 21:22:13 +0800

    class

--- class-note/class.js
+++ class-note/class.js
@@ -1,3 +1,8 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+var capt = new Person('캡틴', 100);
 class Person {
     // 클래스 로직
     constructor(name, age) {
@@ -10,5 +15,3 @@ class Person {
 
 var seho = new Person('세호', 30);
 console.log(seho);
-
-var capt = new Person(30, 30);
