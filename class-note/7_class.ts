8eb1c6c3f06521aa0b2cb7d9d3b0ebae40673a98
dlstodyo <dudgks3316@naver.com>
2022-02-09 19:49:30 +0900

    class

--- class-note/7_class.ts
+++ class-note/7_class.ts
@@ -1,7 +1,7 @@
 class Person {
     private name: string;
     public age: number;
-    readonly log: string;
+    readonly log: string; // 읽기만 가능 (값 변경 불가)
 
     constructor(name: string, age: number) {
         this.name = name;
@@ -9,10 +9,20 @@ class Person {
     }
 }
 
+// 리액트 예전 문법 - 클래스 기반 코드
 class App extends React.Component {
 
 }
 
+// 리액트 최신 문법 - 훅 기반의 함수형 코드
 function App() {
     return <div>Hello World</div>
 }
+
+//
+new Vue({
+    el: '',
+    setup() {
+
+    }
+})
