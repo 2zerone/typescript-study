8d5620ac22407f0df7efb8e5879fdca3e637b105
dlstodyo <dudgks3316@naver.com>
2022-02-25 18:54:37 +0900

    generics

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -14,16 +14,18 @@
 
 // logText<string>('하이');
 
-// function logText(text: string) {
-//     console.log(text);
-//     text.split('').reverse().join('');
-//     return text;
-// }
+// 실제로 함수를 정의할 때 타입을 비워놓은 상태에서 타입에 무엇이 들어갈 것인가를 호출한 시점에 정의하는 것 = 제네릭의 장점
 
-// function logNumber(num: number) {
-//     console.log(num);
-//     return num;
-// }
+function logText(text: string) {
+    console.log(text);
+    // text.split('').reverse().join(''); // string이기 때문에 가능
+    return text;
+}
+
+function logNumber(num: number) {
+    console.log(num);
+    return num;
+}
 
 // function logText(text: string | number) {
 //     console.log(text);
@@ -40,7 +42,9 @@ function logText<T>(text: T): T {
     return text;
 }
 
-logText<string>('abc')
+const str = logText<string>('abc')
+str.split('')
+const login = logText<boolean>(true);
 
 // logText('a')
 // logText(10)
