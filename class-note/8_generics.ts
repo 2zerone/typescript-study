adee1d2fe6d0cb59a3965035273530b69f979de3
2zerone <dudgks3316@naver.com>
2022-02-26 16:17:44 +0900

    generics

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -48,3 +48,58 @@ const login = logText<boolean>(true);
 
 // logText('a')
 // logText(10)
+
+// 인터페이스에 제네릭 선언하는 방법
+// interface Dropdown {
+//     value: string;
+//     selected: boolean;
+// }
+
+// const obj: Dropdown = { value: '하이', selected: false };
+
+interface Dropdown<T> {
+    value: T;
+    selected: boolean;
+}
+
+const obj: Dropdown<number> = { value: 10, selected: false };
+
+// 제네릭의 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//     console.log(text.length);
+//     text.forEach(function (text) {
+//         console.log(text);
+//     })
+//     return text;
+// }
+// logTextLength<string>(['hi', 'abc']);
+
+// 제네릭의 타입nds  제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+
+function logTextLength<T extends LengthType>(text: T): T {
+    text.length;
+    return text;
+}
+
+logTextLength('a');
+logTextLength(10); // 10 에는 기본적으로 length가 제공되지 않음
+logTextLength({ leng: 10 });
+
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+    return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption('name');
